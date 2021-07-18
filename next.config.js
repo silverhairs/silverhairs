cf4dccf733704fff03f6de95cd06c423096f1257
Boris Kayi <boriskayienzo@gmail.com>
2021-07-19 01:00:23 +0200

    chore: optimization for next/image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
   reactStrictMode: true,
   env: {
     spotifyToken:
       "BQDy1siyKWP9BPghl4bgSNexDk-i_CwYygV6vRA3BU2lWhxRbD8uxSLKgcXLBGcYG5KfJccpSDnp40GKX-qGrHYSJn63RECfBCkqCo3yPyOXS4RiI_6zetLPDSA_2rMT4_qdLIBKbGikR5UnxqKchm6Tr3GbRg_qCmKq2sc",
   },
-};
+});
