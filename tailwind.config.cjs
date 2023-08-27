76a346c130687ece83677b6f1cba281ec048806d
Boris Kayi <hello@silverhairs.engineer>
2023-08-27 17:50:38 +0200

    feat: redesign header

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        monospace: ['Overpass Mono Variable'],
+        sans: ['Inter Variable', 'Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
