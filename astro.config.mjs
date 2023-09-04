163c0c7b5c9b73274ec8f9bda13f59f1341478b6
Boris Kayi <hello@silverhairs.engineer>
2023-09-05 00:44:57 +0200

    chore: configure vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
-import { Config } from "prettier-plugin-astro";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()],
+  output:"static",
+  adapter:vercelStatic(),
 });
