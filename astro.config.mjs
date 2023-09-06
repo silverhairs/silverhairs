dfdddfeddd088489d6067f4301c7beb580d8307b
Boris Kayi <hello@silverhairs.engineer>
2023-09-06 20:58:54 +0200

    feat: add Turnstile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()],
-  output:"static",
-  adapter:vercelStatic(),
+  output: "server",
+  adapter: vercel()
 });
