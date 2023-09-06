3f735665ad5fc39e4aa02481c2507d58868c8c22
Boris Kayi <hello@silverhairs.engineer>
2023-09-06 21:08:28 +0200

    fix: vercel deploy failuer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercelEdge from '@astrojs/vercel/edge';
 
-// https://astro.build/config
+
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercelEdge(),
 });
